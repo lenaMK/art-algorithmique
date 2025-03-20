a123b6b36b87bc67378efaccc06a7b2a9df12b68
Lena Krause <lena.krause@umontreal.ca>
2025-03-20 15:28:32 -0400

    init and MAC data

---
+++ exSynth/doc.md
@@ -0,0 +1,4 @@
+Documentation
+
+Téléchargement des données le 20 mars 2025: https://www.donneesquebec.ca/recherche/dataset/macrepertoire
+
---
+++ exSynth/sketch.js
@@ -0,0 +1,24 @@
+/*
+    title: 
+    author: LenaMK
+    date: 2025-03-20
+    description: 
+    data source: https://www.donneesquebec.ca/recherche/dataset/macrepertoire
+    notes: 
+
+*/
+
+function preload(){
+
+}
+
+
+function setup(){
+
+
+}
+
+function draw() {
+
+
+}
